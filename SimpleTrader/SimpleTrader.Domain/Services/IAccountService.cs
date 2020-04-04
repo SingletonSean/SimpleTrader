e03747c402a4483ac12c02b9a5d5ac6b78780ff4
SingletonSean <bballheat6@gmail.com>
2020-04-04 11:02:11 -0400

    Add authentication service

---
+++ SimpleTrader/SimpleTrader.Domain/Services/IAccountService.cs
@@ -0,0 +1,14 @@
+﻿using SimpleTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.Domain.Services
+{
+    public interface IAccountService : IDataService<Account>
+    {
+        Task<Account> GetByUsername(string username);
+        Task<Account> GetByEmail(string email);
+    }
+}
--- SimpleTrader/SimpleTrader.WPF/App.xaml.cs
+++ SimpleTrader/SimpleTrader.WPF/App.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using SimpleTrader.Domain.Models;
 using SimpleTrader.Domain.Services;
+using SimpleTrader.Domain.Services.AuthenticationServices;
 using SimpleTrader.Domain.Services.TransactionServices;
 using SimpleTrader.EntityFramework;
 using SimpleTrader.EntityFramework.Services;
@@ -26,6 +28,8 @@ namespace SimpleTrader.WPF
         protected override void OnStartup(StartupEventArgs e)
         {
             IServiceProvider serviceProvider = CreateServiceProvider();
+            IAuthenticationService authentication = serviceProvider.GetRequiredService<IAuthenticationService>();
+            authentication.Login("SingletonSean", "Test123");
 
             Window window = serviceProvider.GetRequiredService<MainWindow>();
             window.Show();
@@ -38,10 +42,14 @@ namespace SimpleTrader.WPF
             IServiceCollection services = new ServiceCollection();
 
             services.AddSingleton<SimpleTraderDbContextFactory>();
+            services.AddSingleton<IAuthenticationService, AuthenticationService>();
             services.AddSingleton<IDataService<Account>, AccountDataService>();
+            services.AddSingleton<IAccountService, AccountDataService>();
             services.AddSingleton<IStockPriceService, StockPriceService>();
             services.AddSingleton<IBuyStockService, BuyStockService>();
             services.AddSingleton<IMajorIndexService, MajorIndexService>();
+
+            services.AddSingleton<IPasswordHasher, PasswordHasher>();
 
             services.AddSingleton<IRootSimpleTraderViewModelFactory, RootSimpleTraderViewModelFactory>();
             services.AddSingleton<ISimpleTraderViewModelFactory<HomeViewModel>, HomeViewModelFactory>();
