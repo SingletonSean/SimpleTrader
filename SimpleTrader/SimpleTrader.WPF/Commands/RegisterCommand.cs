7be6bfb9b6f43c6c3d35822b6f96b2483367b33f
SingletonSean <bballheat6@gmail.com>
2020-09-02 21:06:49 -0400

    Add registration form

---
+++ SimpleTrader/SimpleTrader.WPF/Commands/RegisterCommand.cs
@@ -0,0 +1,62 @@
+﻿using SimpleTrader.Domain.Services.AuthenticationServices;
+using SimpleTrader.WPF.State.Authenticators;
+using SimpleTrader.WPF.State.Navigators;
+using SimpleTrader.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.WPF.Commands
+{
+    public class RegisterCommand : AsyncCommandBase
+    {
+        private readonly RegisterViewModel _registerViewModel;
+        private readonly IAuthenticator _authenticator;
+        private readonly IRenavigator _registerRenavigator;
+
+        public RegisterCommand(RegisterViewModel registerViewModel, IAuthenticator authenticator, IRenavigator registerRenavigator)
+        {
+            _registerViewModel = registerViewModel;
+            _authenticator = authenticator;
+            _registerRenavigator = registerRenavigator;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            _registerViewModel.ErrorMessage = string.Empty;
+
+            try
+            {
+                RegistrationResult registrationResult = await _authenticator.Register(
+                       _registerViewModel.Email,
+                       _registerViewModel.Username,
+                       _registerViewModel.Password,
+                       _registerViewModel.ConfirmPassword);
+
+                switch (registrationResult)
+                {
+                    case RegistrationResult.Success:
+                        _registerRenavigator.Renavigate();
+                        break;
+                    case RegistrationResult.PasswordsDoNotMatch:
+                        _registerViewModel.ErrorMessage = "Password does not match confirm password.";
+                        break;
+                    case RegistrationResult.EmailAlreadyExists:
+                        _registerViewModel.ErrorMessage = "An account for this email already exists.";
+                        break;
+                    case RegistrationResult.UsernameAlreadyExists:
+                        _registerViewModel.ErrorMessage = "An account for this username already exists.";
+                        break;
+                    default:
+                        _registerViewModel.ErrorMessage = "Registration failed.";
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                _registerViewModel.ErrorMessage = "Registration failed.";
+            }
+        }
+    }
+}
---
+++ SimpleTrader/SimpleTrader.WPF/Commands/RenavigateCommand.cs
@@ -0,0 +1,30 @@
+﻿using SimpleTrader.WPF.State.Navigators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace SimpleTrader.WPF.Commands
+{
+    public class RenavigateCommand : ICommand
+    {
+        private readonly IRenavigator _renavigator;
+
+        public RenavigateCommand(IRenavigator renavigator)
+        {
+            _renavigator = renavigator;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _renavigator.Renavigate();
+        }
+    }
+}
