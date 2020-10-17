595ff99077b7c7fe177b882461b5c1f31563a8c0
SingletonSean <bballheat6@gmail.com>
2020-10-16 20:27:16 -0400

    Add sell stock service

---
+++ SimpleTrader/SimpleTrader.Domain.Tests/Services/TransactionServices/BuyStockServiceTests.cs
@@ -0,0 +1,115 @@
+﻿using Moq;
+using NUnit.Framework;
+using SimpleTrader.Domain.Exceptions;
+using SimpleTrader.Domain.Models;
+using SimpleTrader.Domain.Services;
+using SimpleTrader.Domain.Services.TransactionServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.Domain.Tests.Services.TransactionServices
+{
+    [TestFixture]
+    public class BuyStockServiceTests
+    {
+        private Mock<IStockPriceService> _mockStockPriceService;
+        private Mock<IDataService<Account>> _mockAccountService;
+        private BuyStockService _buyStockService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockStockPriceService = new Mock<IStockPriceService>();
+            _mockAccountService = new Mock<IDataService<Account>>();
+
+            _buyStockService = new BuyStockService(_mockStockPriceService.Object, _mockAccountService.Object);
+        }
+
+        [Test]
+        public void BuyStock_WithInvalidSymbol_ThrowsInvalidSymbolExceptionForSymbol()
+        {
+            string expectedInvalidSymbol = "bad_symbol";
+            _mockStockPriceService.Setup(s => s.GetPrice(expectedInvalidSymbol)).ThrowsAsync(new InvalidSymbolException(expectedInvalidSymbol));
+
+            InvalidSymbolException excpetion = Assert.ThrowsAsync<InvalidSymbolException>(
+                () => _buyStockService.BuyStock(It.IsAny<Account>(), expectedInvalidSymbol, It.IsAny<int>()));
+            string actualInvalidSymbol = excpetion.Symbol;
+
+            Assert.AreEqual(expectedInvalidSymbol, actualInvalidSymbol);
+        }
+
+        [Test]
+        public void BuyStock_WithGetPriceFailure_ThrowsException()
+        {
+            _mockStockPriceService.Setup(s => s.GetPrice(It.IsAny<string>())).ThrowsAsync(new Exception());
+
+            Assert.ThrowsAsync<Exception>(
+                () => _buyStockService.BuyStock(It.IsAny<Account>(), It.IsAny<string>(), It.IsAny<int>()));
+        }
+
+        [Test]
+        public void BuyStock_WithInsufficientFunds_ThrowsInsufficientFundsExceptionForBalances()
+        {
+            double expectedAccountBalance = 0;
+            double expectedRequiredBalance = 100;
+            Account buyer = CreateAccount(expectedAccountBalance);
+            _mockStockPriceService.Setup(s => s.GetPrice(It.IsAny<string>())).ReturnsAsync(expectedRequiredBalance);
+
+            InsufficientFundsException exception = Assert.ThrowsAsync<InsufficientFundsException>(
+                () => _buyStockService.BuyStock(buyer, It.IsAny<string>(), 1));
+            double actualAccountBalance = exception.AccountBalance;
+            double actualRequiredBalance = exception.RequiredBalance;
+
+            Assert.AreEqual(expectedAccountBalance, actualAccountBalance);
+            Assert.AreEqual(expectedRequiredBalance, actualRequiredBalance);
+        }
+
+        [Test]
+        public void BuyStock_WithAccountUpdateFailure_ThrowsException()
+        {
+            Account buyer = CreateAccount(1000);
+            _mockStockPriceService.Setup(s => s.GetPrice(It.IsAny<string>())).ReturnsAsync(100);
+            _mockAccountService.Setup(s => s.Update(It.IsAny<int>(), It.IsAny<Account>())).Throws(new Exception());
+
+            Assert.ThrowsAsync<Exception>(() => _buyStockService.BuyStock(buyer, It.IsAny<string>(), 1));
+        }
+
+        [Test]
+        public async Task BuyStock_WithSuccessfulPurchase_ReturnsAccountWithNewTransaction()
+        {
+            int expectedTransactionCount = 1;
+            Account buyer = CreateAccount(1000);
+            _mockStockPriceService.Setup(s => s.GetPrice(It.IsAny<string>())).ReturnsAsync(100);
+
+            buyer = await _buyStockService.BuyStock(buyer, It.IsAny<string>(), 1);
+            int actualTransactionCount = buyer.AssetTransactions.Count();
+
+            Assert.AreEqual(expectedTransactionCount, actualTransactionCount);
+        }
+
+        [Test]
+        public async Task BuyStock_WithSuccessfulPurchase_ReturnsAccountWithNewBalance()
+        {
+            double expectedBalance = 0;
+            Account buyer = CreateAccount(100);
+            _mockStockPriceService.Setup(s => s.GetPrice(It.IsAny<string>())).ReturnsAsync(50);
+
+            buyer = await _buyStockService.BuyStock(buyer, It.IsAny<string>(), 2);
+            double actualBalance = buyer.Balance;
+
+            Assert.AreEqual(expectedBalance, actualBalance);
+        }
+
+        private Account CreateAccount(double balance)
+        {
+            return new Account()
+            {
+                Balance = balance,
+                AssetTransactions = new List<AssetTransaction>()
+            };
+        }
+    }
+}
---
+++ SimpleTrader/SimpleTrader.Domain/Services/TransactionServices/SellStockService.cs
@@ -0,0 +1,60 @@
+﻿using SimpleTrader.Domain.Exceptions;
+using SimpleTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.Domain.Services.TransactionServices
+{
+    public class SellStockService : ISellStockService
+    {
+        private readonly IStockPriceService _stockPriceService;
+        private readonly IDataService<Account> _accountService;
+
+        public SellStockService(IStockPriceService stockPriceService, IDataService<Account> accountService)
+        {
+            _stockPriceService = stockPriceService;
+            _accountService = accountService;
+        }
+
+        public async Task<Account> SellStock(Account seller, string symbol, int shares)
+        {
+            // Validate seller has sufficient shares.
+            int accountShares = GetAccountSharesForSymbol(seller, symbol);
+            if(accountShares < shares)
+            {
+                throw new InsufficientSharesException(symbol, accountShares, shares);
+            }
+
+            double stockPrice = await _stockPriceService.GetPrice(symbol);
+
+            seller.AssetTransactions.Add(new AssetTransaction()
+            {
+                Account = seller,
+                Asset = new Asset()
+                {
+                    PricePerShare = stockPrice,
+                    Symbol = symbol
+                },
+                DateProcessed = DateTime.Now,
+                IsPurchase = false,
+                Shares = shares
+            });
+
+            seller.Balance += stockPrice * shares;
+
+            await _accountService.Update(seller.Id, seller);
+
+            return seller;
+        }
+
+        private int GetAccountSharesForSymbol(Account seller, string symbol)
+        {
+            IEnumerable<AssetTransaction> accountTransactionsForSymbol = seller.AssetTransactions.Where(a => a.Asset.Symbol == symbol);
+            
+            return accountTransactionsForSymbol.Sum(a => a.IsPurchase ? a.Shares : -a.Shares);
+        }
+    }
+}
