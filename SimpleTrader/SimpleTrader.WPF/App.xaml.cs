c0d4c6fa692f2866e1c6c16c936ca37dd0e69da1
SingletonSean <bballheat6@gmail.com>
2020-12-06 09:19:27 -0500

    Cleanup usings

--- SimpleTrader/SimpleTrader.WPF/App.xaml.cs
+++ SimpleTrader/SimpleTrader.WPF/App.xaml.cs
@@ -1,37 +1,13 @@
-﻿using Microsoft.AspNet.Identity;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SimpleTrader.Domain.Models;
-using SimpleTrader.Domain.Services;
-using SimpleTrader.Domain.Services.AuthenticationServices;
-using SimpleTrader.Domain.Services.TransactionServices;
 using SimpleTrader.EntityFramework;
-using SimpleTrader.EntityFramework.Services;
-using SimpleTrader.FinancialModelingPrepAPI;
-using SimpleTrader.FinancialModelingPrepAPI.Services;
 using SimpleTrader.WPF.HostBuilders;
-using SimpleTrader.WPF.State.Accounts;
-using SimpleTrader.WPF.State.Assets;
-using SimpleTrader.WPF.State.Authenticators;
-using SimpleTrader.WPF.State.Navigators;
-using SimpleTrader.WPF.ViewModels;
-using SimpleTrader.WPF.ViewModels.Factories;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Navigation;
 
 namespace SimpleTrader.WPF
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
         private readonly IHost _host;
